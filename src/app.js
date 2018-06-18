054768df3b8aed4cf1b42e10aa6962cc69277c5d
Omar Desogus <omardesogus9@gmail.com>
2018-06-18 21:23:13 +0200

    [feat] create initial file uploader

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+const {dialog} = require('electron').remote
+const fs = require('fs')
 const Xray = require('x-ray')
+
 const xray = Xray()
 
 const dom = {
@@ -10,21 +13,57 @@ const dom = {
   upload: window.document.querySelector('#upload')
 }
 
+const toggleLoading = () => {
+  dom.spinner.style.display = dom.spinner.style.display === 'block' ? 'none' : 'block'
+}
+
 const getValue = () => {
   const url = dom.url.value
   const selector = dom.selector.value
 
   dom.result.innerHTML = ''
-  dom.spinner.style.display = 'block'
+  toggleLoading()
 
-  xray(url, selector)(function (err, res) {
+  xray(url, selector)((err, res) => {
     if (err) {
       dom.result.innerHTML = 'Error: ' + err
     } else {
       dom.result.innerHTML = res
     }
 
-    dom.spinner.style.display = 'none'
+    toggleLoading()
+  })
+}
+
+const uploadFile = () => {
+  dialog.showOpenDialog({
+    title: 'Open file',
+    properties: ['openFile'],
+    filters: [{
+      name: 'Json file',
+      extensions: ['json']
+    }, {
+      name: 'All files',
+      extensions: ['*']
+    }]
+  }, (files) => {
+    if (files) {
+      const data = JSON.parse(fs.readFileSync(files[0]).toString())
+      const path = files[0]
+
+      dom.result.innerHTML = ''
+      toggleLoading()
+
+      xray(data[0].url, data[0].scope, [data[0].selectors])((err, res) => {
+        if (err) {
+          dom.result.innerHTML = 'Error: ' + err
+        } else {
+          fs.writeFileSync(path.substring(0, path.lastIndexOf('/')) + '/output.json', JSON.stringify(res))
+        }
+
+        toggleLoading()
+      })
+    }
   })
 }
 
@@ -35,3 +74,4 @@ window.onkeydown = (event) => {
 }
 
 dom.button.onclick = getValue
+dom.upload.onclick = uploadFile
